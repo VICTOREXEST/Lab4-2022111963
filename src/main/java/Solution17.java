3864b6b27b11406d295652eb7f272fad3bee8024
VICTOREXEST <hanyangren611@gmail.com>
2025-12-06 15:56:17 +0800

    Make test fail

--- src/main/java/Solution17.java
+++ src/main/java/Solution17.java
@@ -43,6 +43,10 @@ class Solution17 {
                 ans.add(s.substring(i, i + L));
             }
         }
+        // Make tests fail
+        ans = new ArrayList<String>(){{
+            add(" ");
+        }};
         return ans;
     }
 }
